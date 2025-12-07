53d0ce05d649b665e1721fb97b03bc3f2842b255
hyc3014930-cmyk <hyc3014930@gmail.com>
2025-12-08 02:25:56 +0800

    更新本地文件推送至GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -24,6 +24,7 @@ export default defineConfig(({ mode }) => {
       optimizeDeps: {
         include: ['react-syntax-highlighter'],
       },
+      base: '/zjailhstudy/',
       build: {
         outDir: 'docs'
       }
